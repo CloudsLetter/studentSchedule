026be778242e4843b28b8f28da3f300c9af3e821
ZhiHua Li <53941684+CloudsLetter@users.noreply.github.com>
2025-01-09 01:58:53 +0800

    更新说明，防呆

--- provider.js
+++ provider.js
@@ -6,8 +6,7 @@ async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom =
   1. 请先在教务系统中选择学年学期。接着再选择好周数，通常来说为1-16/18周，具体请咨询同学、老师、教务处
   2. 点击查询。
   3. 点击一键导入。
-  4. 如遇查询失败请不要慌张，将界面滑动至右上角点击返回首页此时出现重新登录弹窗，此时只需点击重新登录按钮即可
-
+  4. 如遇查询失败请不要慌张，将界面滑动至右上角点击返回首页此时出现重新登录弹窗，此时只需点击重新登录按钮即可。
   `);
   
   const element = dom.querySelector('.ant-spin-container');
