2752f40c2eb45c846f583c50243841f95be6866b
ZhiHua Li <53941684+CloudsLetter@users.noreply.github.com>
2025-01-09 01:51:33 +0800

    更新说明

--- provider.js
+++ provider.js
@@ -2,9 +2,10 @@ async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom =
   await loadTool('AIScheduleTools');
   await AIScheduleAlert(`
   🎉导入流程🎉
+  1. 请先进行登录操作，本次登录不会保存您的任何账户信息，所有信息均在您的设备上进行处理。
   1. 请先在教务系统中选择学年学期。接着再选择好周数，通常来说为1-16/18周，具体请咨询同学、老师、教务处
-  2. 点击查询
-  3. 点击一键导入
+  2. 点击查询。
+  3. 点击一键导入。
   `);
   
   const element = dom.querySelector('.ant-spin-container');
